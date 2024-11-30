a1443e25311c70cf0d9b711a7eb987eb4c0b4120
sfmcmullen <sfmcmullen@gmail.com>
2024-11-30 18:16:53 +0000

    Add test cases for Card class and validate card values

--- test/CardTest.java
+++ test/CardTest.java
@@ -9,5 +9,11 @@ public class CardTest {
     public void testGetValue() {
         card = new Card(5);
         assertEquals(5, card.getValue(), "Card value should be 5");
+
+        card = new Card(10);
+        assertEquals(10, card.getValue(), "Card value should be 10");
+
+        card = new Card(1);
+        assertEquals(1, card.getValue(), "Card value should be 1");
     }
 }
